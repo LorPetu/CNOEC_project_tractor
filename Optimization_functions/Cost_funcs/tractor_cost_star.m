68958243823045528a4f512602ee342796f08994
LorPetu <100023607+LorPetu@users.noreply.github.com>
2024-08-20 12:07:52 +0200

    Deleted dprecated files

--- Optimization_functions/Cost_funcs/tractor_cost_star.m
+++
@@ -1,5 +0,0 @@
-function [f] = tractor_cost_star(U,z0,zf,Ts,Np,parameters,Optimization_opt)
-[F] = tractor_cost_GN_grad(U,z0,zf,Ts,Np,parameters,Optimization_opt);
-
-
-f=F'*F;
--- Optimization_functions/Cost_funcs/tractor_cost_star_mod.m
+++
@@ -1,5 +0,0 @@
-function [f] = tractor_cost_star_mod(U,z0,zf,Np,Ns,parameters,Optimization_opt)
-[F] = tractor_cost_GN_grad_mod(U,z0,zf,Np,Ns,parameters,Optimization_opt);
-
-
-f=F'*F;
